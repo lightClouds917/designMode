6a94328f31d78df5a13b6647638cbb5f90014134
IT云清 <you@example.com>
2019-06-05 11:52:01 +0800

    初始化

---
+++ src/main/java/com/java4all/DesignmodeApplication.java
@@ -0,0 +1,16 @@
+package com.java4all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * IT云清
+ */
+@SpringBootApplication
+public class DesignmodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignmodeApplication.class, args);
+	}
+
+}

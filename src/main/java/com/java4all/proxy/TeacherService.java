7d6beba3709aa54bbb7743f5b2e0355d09b22d2c
lightClouds917 <you@example.com>
2019-06-07 22:18:38 +0800

    优化

--- src/main/java/com/java4all/proxy/TeacherService.java
+++ src/main/java/com/java4all/proxy/TeacherService.java
@@ -4,6 +4,7 @@ package com.java4all.proxy;
  * @author IT云清
  * @date 2019年01月01日 11:11:11
  */
+@SuppressWarnings("all")
 public class TeacherService {
     String doFirst(){
         return "doFirst返回";

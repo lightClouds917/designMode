a80a880a37d619b42cda6b7cf241886fb72245ce
lightClouds917 <1186355422@qq.com>
2019-06-08 15:28:16 +0800

    add 工厂模式

--- src/main/java/com/java4all/factory/Car.java
+++ src/main/java/com/java4all/factory/Car.java
@@ -1,10 +1,19 @@
 package com.java4all.factory;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 工厂模式
  * @author IT云清
  * @date 2019年01月01日 11:11:11
  */
+@Slf4j
 public class Car {
 
+    /**种类*/
+    private Integer type;
+
+    /**颜色*/
+    private String colour;
 }
+

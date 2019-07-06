7f1187d374a940d3c94e6dfe84e868e890a82e1c
lightClouds917 <1186355422@qq.com>
2019-07-06 17:15:46 +0800

    add 责任链模式

---
+++ src/test/java/com/java4all/responsibility/ResponsibilityTest.java
@@ -0,0 +1,29 @@
+package com.java4all.responsibility;
+
+import org.junit.Test;
+
+/**
+ * 测试责任链模式
+ * @author wangzhongxiang
+ * @date 2019年07月06日 17:09:19
+ */
+public class ResponsibilityTest {
+
+    @Test
+    public void testResponsibility(){
+        AbstractLogger loggerChain = this.getChainOfLoggers();
+        loggerChain.logMessage(AbstractLogger.ERROR,"这里出error级别的问题了");
+        loggerChain.logMessage(AbstractLogger.INFO,"这里出info级别的问题了");
+    }
+
+    private AbstractLogger getChainOfLoggers(){
+        InfoLogger infoLogger = new InfoLogger(AbstractLogger.INFO);
+        DebugLogger debugLogger = new DebugLogger(AbstractLogger.DEBUG);
+        ErrorLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+
+        errorLogger.setNextAbstractLogger(debugLogger);
+        debugLogger.setNextAbstractLogger(infoLogger);
+
+        return errorLogger;
+    }
+}

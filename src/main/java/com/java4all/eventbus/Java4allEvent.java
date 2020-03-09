433b7060da7e1d926512fb77b2acad95a3a02978
lightClouds917 <1186355422@qq.com>
2020-03-09 16:55:39 +0800

    add:test

--- src/main/java/com/java4all/eventbus/Java4allEvent.java
+++ src/main/java/com/java4all/eventbus/Java4allEvent.java
@@ -4,6 +4,13 @@ package com.java4all.eventbus;
  * https://www.cnblogs.com/peida/p/EventBus.html
  * 消息封装类
  * @author IT云清
+ *
+ * description:
+ * 1.创建消息封装对象Java4allEvent；
+ * 2.创建订阅者Java4allEventListener，添加接受消息后的处理逻辑；
+ * 3.创建evenbus对象，订阅者向eventnbus注册listener;
+ * 4.eventbut post推送消息；
+ * 5.订阅者接收到eventbus推送的信息后做处理；
  */
 public class Java4allEvent {
     private final String message;
